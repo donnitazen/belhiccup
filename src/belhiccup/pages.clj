f7f58626ba7abaeef13978d9e9803c8b85ee3f4e
squest <sabdaps@gmail.com>
2015-09-22 21:05:23 +0700

    some edits

--- src/belhiccup/pages.clj
+++ src/belhiccup/pages.clj
@@ -6,89 +6,91 @@
 
 (defn- head
   [title]
-  (hc/html [:head
-            [:title title]
-            [:meta {:charset "utf-8"}]
-            [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"}]
-            [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"}]
-            [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"}]]))
+  [:head
+   [:title title]
+   [:meta {:charset "utf-8"}]
+   [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"}]
+   [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"}]
+   [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"}]])
 
 (defn- header
   []
-  (hc/html [:header
-            [:ul {:class "nav nav-pills"}
-             [:li {:role "presentation"}
-              [:a {:href "/"} "Belhiccup"]]
-             [:li {:role "presentation"}
-              [:a {:href "/articles"} "Articles"]]]]))
+  [:header
+   [:ul {:class "nav nav-pills"}
+    [:li {:role "presentation"}
+     [:a {:href "/"} "Belhiccup"]]
+    [:li {:role "presentation"}
+     [:a {:href "/articles"} "Articles"]]]])
 
 (defn- body
   [anything]
-  (hc/html [:body {:class "container"}
-            [:div {:class "row"}
-             (header)]
-            anything
-            [:div {:class "row"}
-             (footer)]]))
+  [:body {:class "container"}
+   [:div {:class "row"}
+    (header)]
+   anything
+   [:div {:class "row"}
+    (footer)]])
 
 (defn- footer
   []
-  (hc/html [:div {:class "panel-footer"}
-            [:div {:class "col-md-3"}
-             [:a {:href "/contact"} "Contact Me"]]]))
+  [:div {:class "panel-footer"}
+   [:div {:class "col-md-3"}
+    [:a {:href "/contact"} "Contact Me"]]])
 
 (defn home
   []
-  (hp/html5 (head "Belhiccup - Home")
-            (body [:div {:class "row"}
-                   [:div {:class "jumbotron"}
-                    [:h1 "This should be a cool jumbo pictures"]]
-                   [:p "body"]])))
+  (hp/html5
+   (head "Belhiccup - Home")
+   (body [:div {:class "row"}
+          [:div {:class "jumbotron"}
+           [:h1 "This should be a cool jumbo pictures"]]
+          [:p "body"]])))
 
 (defn contact
   []
-  (hp/html5 (head "Belhiccup - Contact")
-            (body [:div {:class "row"}
-                   [:p "just call"]])))
+  (hp/html5
+   (head "Belhiccup - Contact")
+   (body [:div {:class "row"}
+          [:p "just call"]])))
 
 (defn map-all-articles
   []
-  (hc/html
-    (for [arts (art/read-article-file "articles.edn")
-                 ]
-             (#(vector :li {:role "presentation"}
-                       (vector :a {:href (str "/article/" (% :article-id))} (% :title))) arts))))
+  (for [art (art/read-article-file "articles.edn")]
+    [:li {:role "presentation"}
+     [:a {:href (str "/article/" (art :article-id))} (art :title)]]))
 
 (defn articles
   []
-  (hp/html5 (head "Belhiccup All Articles")
-           (body [:div {:class "row"}
-                  [:div {:class "col-md-2"}
-                   [:ul {:class "nav nav-pills nav-stacked"}
-                    (map-all-articles)]]
-                  [:div {:class "col-md-10"}
-                   [:h3 "Some cool quotes to seize the day"]
-                   [:button {:type "submit"
-                             :class "btn btn-default"}
-                    "Add an article"]]])))
+  (hp/html5
+   (head "Belhiccup All Articles")
+   (body [:div {:class "row"}
+          [:div {:class "col-md-2"}
+           [:ul {:class "nav nav-pills nav-stacked"}
+            (map-all-articles)]]
+          [:div {:class "col-md-10"}
+           [:h3 "Some cool quotes to seize the day"]
+           [:button {:type "submit"
+                     :class "btn btn-default"}
+            "Add an article"]]])))
 
 
 (defn article
   [id]
   (let [the-article (art/show-one-article id)]
     (println the-article)
-    (hp/html5 (head (str "article #" id))
-              (body [:div {:class "row"}
-                     [:div {:class "col-md-2"}
-                      [:ul {:class "nav nav-pills nav-stacked"}
-                       (map-all-articles)]]
-                     [:div {:class "col-md-10"}
-                      [:h2 (str "Title: " (the-article :title))]
-                      [:br]
-                      [:h4 (str "Article #" (the-article :article-id))]
-                      [:br]
-                      [:h5 (str "Content: " (the-article :text))]
-                      [:br]]]))))
+    (hp/html5
+     (head (str "article #" id))
+     (body [:div {:class "row"}
+            [:div {:class "col-md-2"}
+             [:ul {:class "nav nav-pills nav-stacked"}
+              (map-all-articles)]]
+            [:div {:class "col-md-10"}
+             [:h2 (str "Title: " (the-article :title))]
+             [:br]
+             [:h4 (str "Article #" (the-article :article-id))]
+             [:br]
+             [:h5 (str "Content: " (the-article :text))]
+             [:br]]]))))
 
 
 
